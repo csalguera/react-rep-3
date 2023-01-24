2c1b946c56edb2e5045710cda2164e0d21c7fdac
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-24 00:39:20 -0500

    import nested components

--- src/Animalia.jsx
+++ src/Animalia.jsx
@@ -1,3 +1,9 @@
+import Amphibians from "./Amphibians"
+import Birds from "./Birds"
+import Fish from "./Fish"
+import Mammals from "./Mammals"
+import Reptiles from "./Reptiles"
+
 const Animalia = (props) => {
   return (
     <>
--- src/LifeKingdoms.jsx
+++ src/LifeKingdoms.jsx
@@ -1,3 +1,7 @@
+import Animalia from "./Animalia"
+import Fungi from "./Fungi"
+import Plantae from "./Plantae"
+
 const LifeKingdoms = (props) => {
   return (
     <>
